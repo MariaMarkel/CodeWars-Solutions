86997b33568e5f71e9da966f688b548caea3c911
MariaMarkel <volkova0512@gmail.com>
2021-02-18 22:41:58 -0500

    new kata

---
+++ grasshopperCreateRooms.js
@@ -0,0 +1,24 @@
+var rooms = {
+
+    room1 : {
+
+        name: 'Room1',
+        description: 'description1',
+        completed: true,
+    },
+
+    room2 : {
+
+        name: 'Room2',
+        description: 'description2',
+        completed: true,
+    },
+
+    room3 : {
+
+        name: 'Room3',
+        description: 'description3',
+        completed: true,
+    }
+
+}
