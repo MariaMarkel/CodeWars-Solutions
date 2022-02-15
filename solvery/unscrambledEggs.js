5c8b71cf4705850e0a939465f745422d55745503
MariaMarkel <volkova0512@gmail.com>
2022-02-15 13:40:01 -0500

    unscrambledEggs.js

---
+++ solvery/unscrambledEggs.js
@@ -0,0 +1,9 @@
+function unscrambleEggs(word){
+    return word.split('egg').join('')
+}
+
+function unscrambleEggs(word){
+    return word.replace(/egg/g, "");
+}
+
+//remove every 'egg' after each consonant
